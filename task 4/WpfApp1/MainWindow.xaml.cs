a09db8f0ba33bdce2e9489aab210db04add8b92f
Bilalkhan <63426268+bilalkhan219@users.noreply.github.com>
2021-05-06 17:28:16 +0500

    Add files via upload

---
+++ task 4/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (userbox.Text == "Admin" || userbox.Text == "admin")
+            {
+                if (passbox.Password == "Admin" || passbox.Password == "admin")
+                {
+                    MessageBox.Show("Login successful");
+                }
+                else
+                {
+                    MessageBox.Show("Invalid credentials");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Invalid credentials");
+
+            }
+        }
+
+        //private void Button_Click_1(object sender, RoutedEventArgs e)
+        //{
+        //    peekbox.Visibility=Visibility.Visible;
+
+        //}
+
+        private void showbutton_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            peekbox.Text = System.Convert.ToString(passbox.Password);
+            peekbox.Visibility = Visibility.Visible;
+            passbox.Visibility = Visibility.Hidden;
+        }
+
+        private void showbutton_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            peekbox.Visibility = Visibility.Collapsed;
+            passbox.Visibility = Visibility.Visible;
+        }
+    }
+}
